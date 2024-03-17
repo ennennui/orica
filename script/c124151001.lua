fc9d3343214c66424392e7c2ed4a98045e289ab4
Enn Kim <nominate17@gmail.com>
2024-03-17 22:51:51 +0900

    효과 1 수정 - 임뷰먼트 오브 블랙

--- script/c124151001.lua
+++ script/c124151001.lua
@@ -4,10 +4,10 @@ function s.initial_effect(c)
 	c:EnableReviveLimit()
 	--레벨 7 몬스터 1장
 	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsLevel,7),1,1)
-	--마법 / 함정은 효과를 받지 않는다
+	--마법 / 함정은 상대 효과를 받지 않는다
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_IMMUNE_EFFECT)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetTargetRange(LOCATION_ONFIELD,0)
 	e1:SetTarget(function(e,c) return c:IsFaceup() and c:IsSpellTrap() end)
@@ -32,7 +32,7 @@ function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE|LOCATION_REMOVED) and chkc:IsControler(tp) and s.thfilter(chkc) end
 	if chk==0 then return Duel.IsExistingTarget(s.thfilter,tp,LOCATION_GRAVE|LOCATION_REMOVED,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectTarget(tp,s.setfilter,tp,LOCATION_GRAVE|LOCATION_REMOVED,0,1,1,nil)
+	local g=Duel.SelectTarget(tp,s.thfilter,tp,LOCATION_GRAVE|LOCATION_REMOVED,0,1,1,nil)
 	if g:GetFirst():IsLocation(LOCATION_GRAVE) then
 		Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,g,1,tp,0)
 	end
