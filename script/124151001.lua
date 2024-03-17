e2a39610425a3ec85995dd97048f8bac1c6f39ab
Enn Kim <nominate17@gmail.com>
2024-03-17 18:12:51 +0900

    효과 추가 - 임뷰먼트 오브 블랙

--- script/124151001.lua
+++ script/124151001.lua
@@ -4,6 +4,15 @@ function s.initial_effect(c)
 	c:EnableReviveLimit()
 	--레벨 7 몬스터 1장
 	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsLevel,7),1,1)
+	--마법 / 함정은 효과를 받지 않는다
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_IMMUNE_EFFECT)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(LOCATION_ONFIELD,0)
+	e1:SetTarget(function(e,c) return c:IsFaceup() and c:IsSpellTrap() end)
+	e1:SetValue(aux.indoval)
+	c:RegisterEffect(e1)
 	--패에 넣는다
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
